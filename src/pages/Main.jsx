6febe3e04025c5e3d8cf6cb73bc0c1c4700ac6f7
ragOP <najmi.raghib@gmail.com>
2024-05-08 10:42:24 +0530

    that too

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -250,9 +250,9 @@ export default function Main() {
           </div>
           <div className="spots-count">Spots remaining: 4</div>
           <div className="tap-direction">👇 TAP BELOW TO CALL 👇</div>
-          <a href="tel:+18446720874">
+          <a href="tel:+18885170494">
             <div className="call-btn" >
-            CALL (844) 672-0874
+            CALL (888) 517-0494
             </div>
           </a>
           <div className="sub-title">We Have Reserved Your Spot</div>
